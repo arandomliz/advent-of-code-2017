c94e24f05675ddc9cbd35984abd4e7373877aa1b
Elisabeth <elisabeth.s.friedrich@gmail.com>
2017-12-07 18:43:00 +0100

    day 8

---
+++ src/Day08/main.kt
@@ -0,0 +1,53 @@
+package Day08
+
+import java.io.File
+import kotlin.math.max
+
+val src = "src/Day08/"
+
+fun main(args : Array<String>) {
+    println(solution1(File(src + "example01.txt")))
+    println(solution1(File(src + "Day18/input.txt")))
+}
+
+enum class IncDec {
+    inc,
+    dec;
+
+    fun getValue(x: Int): Int = if (this == inc) x else -x
+}
+
+data class SolutionReturnValue(val highest : Int, val highestEver : Int)
+
+fun solution1(file : File) : SolutionReturnValue {
+    val lines = file.readLines()
+
+    val registers = HashMap<String, Int>()
+    val regex = """^(\w+) (inc|dec) (-?\d+) if (\w+) (>=|<=|>|<|!=|==) (-?\d+)$""".toRegex()
+
+    var highestEver = Int.MIN_VALUE
+
+    lines.forEach {
+        val groups = (regex.find(it, 0)?.groupValues ?: listOf()).toTypedArray()
+
+
+        val amount = IncDec.valueOf(groups[2]).getValue(groups[3].toInt())
+        val compareToV = registers.getOrDefault(groups[4], 0).compareTo(groups[6].toInt())
+        val shouldDo = when(groups[5]) {
+            ">=" -> compareToV >= 0
+            ">"  -> compareToV > 0
+            "<=" -> compareToV <= 0
+            "<"  -> compareToV < 0
+            "==" -> compareToV == 0
+            "!=" -> compareToV != 0
+            else -> throw IllegalArgumentException("$it couldn't be read")
+        }
+
+        if (shouldDo) {
+            registers[groups[1]] = registers.getOrDefault(groups[1], 0) + amount
+            highestEver = max(highestEver, registers[groups[1]]!!)
+        }
+    }
+
+    return SolutionReturnValue(registers.maxBy { it.value }!!.value, highestEver)
+}
