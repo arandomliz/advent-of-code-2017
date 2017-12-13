920f2fbbe157bd8977bb0f5f163f34f14b71168d
Elisabeth <elisabeth.s.friedrich@gmail.com>
2017-12-13 14:32:00 +0100

    d 13

---
+++ src/Day13/main.kt
@@ -0,0 +1,56 @@
+package Day13
+
+import java.io.File
+import kotlin.test.assertEquals
+
+val src = "src/Day13/"
+
+fun main(args : Array<String>) {
+    assertEquals(24, solution1(File(src + "example01.txt")))
+    println(solution1(File(src + "Day18/input.txt")))
+
+    assertEquals(10, solution2(File(src + "example01.txt")))
+    println(solution2(File(src + "Day18/input.txt")))
+
+}
+
+fun solution1(file : File) : Int {
+    val record = file.readLines()
+    val regex = """(\d+): (\d+)""".toRegex()
+
+    var severity = 0
+
+    record.forEach {
+        val values = (regex.matchEntire(it)?.groupValues ?: listOf()).subList(1, 3).map { it.toInt() }
+
+        if ( values[0] % ((values[1] - 1) * 2) == 0)
+            severity += values[0] * values[1]
+    }
+
+    return severity
+}
+
+fun gotHit(record : List<IntArray>, delay : Int = 0) : Boolean {
+    record.forEach {
+        if ( (it[0] + delay) % it[1] == 0)
+            return true
+    }
+
+    return false
+}
+
+fun solution2(file : File) : Int {
+    val record = file.readLines()
+    val regex = """(\d+): (\d+)""".toRegex()
+
+    val inputs = record.map {
+        val values = regex.matchEntire(it)?.groupValues ?: listOf()
+        intArrayOf(values[1].toInt(), (values[2].toInt() - 1) * 2)
+    }
+
+    var delay = 0
+    while (gotHit(inputs, delay))
+        delay++
+
+    return delay
+}
